a995c6a9efeeafdb04eb529721360c4caeaf3c42
K'ribean Pizza Gourmet <thewalkingthewire@gmail.com>
2024-06-24 15:39:03 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -37,14 +37,14 @@ function clearCart() {
 
 function placeOrder() {
   const name = prompt("Por favor, ingrese su nombre:");
-  const deliveryOption = prompt("Especifique: '1. Domicilio', '2. Recoger' o '3. En el lugar':");
+  const deliveryOption = prompt("Especifique: 'Domicilio', 'Recoger' o 'En el lugar':");
   let address = '';
-  if (deliveryOption.toLowerCase() === '1. domicilio') {
+  if (deliveryOption.toLowerCase() === 'domicilio') {
     address = prompt("Por favor, ingrese su dirección:");
   }
-  const paymentMethod = prompt("Método de pago: '1. Bancolombia' o '2. Efectivo':");
+  const paymentMethod = prompt("Método de pago: 'Bancolombia' o 'Efectivo':");
   let paymentDetails = '';
-  if (paymentMethod.toLowerCase() === '1. bancolombia') {
+  if (paymentMethod.toLowerCase() === 'bancolombia') {
     paymentDetails = "Haga el depósito a la cuenta de Bancolombia Ahorro a la Mano: 03186687044.";
   }
 
